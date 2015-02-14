6838316617822d4ce438522b71a2d66a4335c679
spectrenoir06 <antoine@doussaud.org>
2015-02-14 01:14:07 +0100

    update

--- main.lua
+++ main.lua
@@ -4,13 +4,21 @@ struct  = require "struct"
 require "json"
 
 local ServerIp = "*"
-local ServerTcpPort = 12345
+local ServerTcpPort = 54321
 
 nb  = 0
 cl  = 0     -- nb de
 dt  = 0
 
 Clients = {}
+
+players = {
+    {x = 0, y = 0},
+    {x = 0, y = 0},
+    {x = 0, y = 0},
+    {x = 0, y = 0},
+    {x = 0, y = 0}
+}
 
 function log(str)
     print(str)
@@ -24,20 +32,22 @@ function handler(skt)
 
     local tcpIp, tcpPort = skt.socket:getpeername()
 
-    Clients[tcpIp..":"..tcpPort] = {ip = tcpIp, port = tcpPort, skt = skt}
+    Clients[tcpIp..":"..tcpPort] = {ip = tcpIp, port = tcpPort, skt = skt, x = 0, y = 0}
     print("new client:\t\t"..tcpIp..":"..tcpPort)
     local me = Clients[tcpIp..":"..tcpPort]
 
     cl = cl +1
     while true do
         nb=nb+1
-        local data, status, partial = skt:receive()
+        local data, status, partial = skt:receive(17)
         --print(data,status, partial)
         if data then
-            print(data)
-            --x,y = struct.unpack("ii", data)
-            --print(x,y)
-            --me.skt:send("hello".."\n")
+        --    print(data)
+            x,y = struct.unpack("dd", data)
+            print(x,y)
+            me.x = x
+            me.y = y
+            me.skt:send(struct.pack("dd", 42.42,43.43))
         end
         if status=="closed" then
             print(status..":\t\t\t"..tcpIp..":"..tcpPort)
